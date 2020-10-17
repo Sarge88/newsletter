e96f4728a33de8644c1df34c95174e8e9f5b10f7
Sarge88 <Sarge88@users.noreply.github.com>
2020-10-17 10:27:10 +0200

    Add data model: Subscription

---
+++ src/main/java/org/marketing/newsletter/model/Subscription.java
@@ -0,0 +1,22 @@
+package org.marketing.newsletter.model;
+
+public class Subscription {
+	
+	private String fullName;
+	private String emailAddress;
+	
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	
+	
+}
