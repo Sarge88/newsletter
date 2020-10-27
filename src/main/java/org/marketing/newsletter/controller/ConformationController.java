8b87bb93a77e3e84de916717b9dd894bb6d0e86c
Sarge88 <kgabor0420@gmail.com>
2020-10-27 09:55:44 +0100

    Add new model to be send data from controller to view

--- src/main/java/org/marketing/newsletter/controller/ConformationController.java
+++ src/main/java/org/marketing/newsletter/controller/ConformationController.java
@@ -1,13 +1,24 @@
 package org.marketing.newsletter.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.marketing.newsletter.model.Newsletter;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ConformationController {
 	
+	private final List<Newsletter> recentNewsletters = Arrays.asList(
+			new Newsletter("Extreme price cut! 20-30-40% OFF!", "..."),
+			new Newsletter("New store in W-London", "..."));
+	
 	@GetMapping("/thank-you")
-	public String getConformation() {
+	public String getConformation(Model model) {
+		model.addAttribute("numberOfNewsletterSubscriptions", 234234); //Dont hardcode this
+		model.addAttribute("newsletterHighlights", recentNewsletters); //Dont hardcode this
 		return "conformation";
 	}
 }
